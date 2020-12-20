7031d1b55d06edad02b21ee673c719fd5546c732
zhj <zhaohaijin@seassoon.com>
2020-12-20 23:39:10 +0800

    30.包含min函数的栈(辅助栈)

---
+++ src/main/java/MinStack.java
@@ -0,0 +1,83 @@
+/**
+ * @Author zhj
+ * @Description 30.包含min函数的栈
+ * @Date 23:38 2020/12/20
+ **/
+//定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+//
+//
+//
+// 示例:
+//
+// MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.min();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.min();   --> 返回 -2.
+//
+//
+//
+//
+// 提示：
+//
+//
+// 各函数的调用总次数不超过 20000 次
+//
+//
+//
+//
+// 注意：本题与主站 155 题相同：https://leetcode-cn.com/problems/min-stack/
+// Related Topics 栈 设计
+// 👍 72 👎 0
+
+
+import java.util.*;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MinStack {
+
+    Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
+
+    /**
+     * initialize your data structure here.
+     */
+    public MinStack() {
+
+    }
+
+    public void push(int x) {
+        if (min >= x) {
+            stack.push(min);
+            min = x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if (min == stack.pop()) {
+            min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.min();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
